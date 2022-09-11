fa8fa04d8cf0efb9709c38e22d49d80f0f84f0ab
CDOMINGUEZ-NTB\Carlos Dominguez <cdominguez@ansuz.la>
2022-09-11 13:29:33 -0300

    cambios asociados a la seccion 2 del curso https://www.udemy.com/course/spring-framework-5/learn/lecture/8276694#overview

--- src/main/java/com/bolsadeideas/springboot/web/app/models/Usuario.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/models/Usuario.java
@@ -1,40 +1,42 @@
 package com.bolsadeideas.springboot.web.app.models;
 
 public class Usuario {
-    private String nombre;
-    private String apellido;
-    private String email;
 
-    public Usuario() {
-    }
+	private String nombre;
+	private String apellido;
+	private String email;
 
-    public Usuario(String nombre, String apellido, String email) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.email = email;
-    }
+	public Usuario() {
+	}
 
-    public String getNombre() {
-        return nombre;
-    }
+	public Usuario(String nombre, String apellido, String email) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.email = email;
+	}
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+	public String getNombre() {
+		return nombre;
+	}
 
-    public String getApellido() {
-        return apellido;
-    }
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
+	public String getApellido() {
+		return apellido;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 }
